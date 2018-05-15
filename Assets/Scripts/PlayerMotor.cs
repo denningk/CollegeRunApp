6ee404c42beb097340731c9042408ac392d6af22
denningk <Keithjake1!>
2018-05-15 14:10:58 -0400

    Created models folder

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+    private CharacterController controller;
+
+    private float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        controller.Move(Vector3.forward * speed * Time.deltaTime);
+	}
+}
