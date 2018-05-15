232055a044bed2efcaed1f9744a7da8ae1bb4519
denningk <Keithjake1!>
2018-05-15 15:52:33 -0400

    animated chad

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMotor : MonoBehaviour {
+public class PlayerMotor : MonoBehaviour
+{
 
     private CharacterController controller;
 
-    private float speed = 5.0f;
+    private float speed = 50.0f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         controller = GetComponent<CharacterController>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         controller.Move(Vector3.forward * speed * Time.deltaTime);
-	}
+    }
 }
