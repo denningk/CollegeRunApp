c613c836a58c7e9356318bf2019335047449a35b
Keith <denningk18@mail.wlu.edu>
2018-05-16 14:10:15 -0400

    upload files

---
+++ Assets/CameraMotor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour {
+
+    private Transform lookAt;
+    private Vector3 startOffset;
+    private Vector3 moveVector;
+
+    private float transition = 0.0f;
+    private float animationDuration = 3.0f;
+    private Vector3 animationOffset = new Vector3(0, 300, 1500);
+
+	// Use this for initialization
+	void Start () {
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+        startOffset = transform.position - lookAt.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveVector = lookAt.position + startOffset;
+
+        // X
+        moveVector.x = 0;
+
+        // Y
+        moveVector.y = Mathf.Clamp(moveVector.y, 200, 400);
+
+        if (transition > 1.0f)
+        {
+            transform.position = moveVector;
+        } else
+        {
+            // Animation at the start of the game
+            transform.position = Vector3.Lerp(moveVector + animationOffset, moveVector, transition);
+            transition += Time.deltaTime / animationDuration;
+            transform.LookAt(lookAt.position - (Vector3.up * 5));
+        }
+
+	}
+}
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -6,8 +6,13 @@ public class PlayerMotor : MonoBehaviour
 {
     public Animator anim;
     private CharacterController controller;
+    private Vector3 moveVector;
 
     private float speed = 500.0f;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 750.0f;
+
+    private float animationDuration = 3.0f;
 
     // Use this for initialization
     void Start()
@@ -19,7 +24,34 @@ public class PlayerMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Time.timeSinceLevelLoad < animationDuration)
+        {
+            anim.Play("Running");
+            controller.Move(Vector3.forward * speed * Time.deltaTime);
+            return;
+        }
         anim.Play("Running");
-        controller.Move(Vector3.forward * speed * Time.deltaTime);
+
+        moveVector = Vector3.zero;
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -0.5f;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        // X - Left and Right
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+
+        // Y - Up and Down
+        moveVector.y = verticalVelocity;
+
+        //Z - Forward and Backward
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);
     }
 }
